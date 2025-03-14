b5fcb945fd0489e3d87b1d5833d7665cf706f3c7
ashok <ashok93.ece@gmail.com>
2025-03-14 17:11:59 +0530

    adding all

--- docker.tf
+++ docker.tf
@@ -6,7 +6,23 @@ resource "aws_instance" "this" {
     volume_size = 50
     volume_type = "gp3"
   }
-  
+provisioner "remote-exec" {
+    inline = [
+      "sudo dnf -y install dnf-plugins-core",
+      "sudo dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo",
+      "sudo dnf install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin",
+      "sudo systemctl enable --now docker",
+      "sudo systemctl start docker",
+      "sudo usermod -aG docker ec2-user"
+    ]
+  }
+  connection {
+    type        = "ssh"
+    user        = "ec2-user"
+    password    = "DevOps321"
+    host        = self.public_ip
+  }
+
   tags = {
     Name    = "docker"
   }
